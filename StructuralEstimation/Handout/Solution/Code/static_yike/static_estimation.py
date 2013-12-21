3ee271fd5b6c8ef5e668699f935a8a5163a975e3
Yike <ywang18@uchicago.edu>
2013-12-20 20:31:46 -0600

    preliminary codes

---
+++ StructuralEstimation/Handout/Solution/Code/static_yike/static_estimation.py
@@ -0,0 +1,86 @@
+''' Estimate the Static Model
+    This project: JG, JEH, YW
+    This code: YW
+    This draft: 20/12/2013
+'''
+
+# Import packages
+import numpy as np
+import scipy.stats
+import os
+import json
+import sys
+from scipy.optimize import fmin_bfgs
+
+# Set working directory
+os.chdir('/Users/ywang/Documents/TA/ECON350_Yike/StructuralEstimation/Handout2/Solution/Code/static')
+
+# Import data
+data = np.genfromtxt('data', dtype = 'float')
+Y = data[:,0:3]
+Z = data[:,3:6]
+Kappa = data[:,6:9]
+nn = data[:,9:12]
+D = data[:,12:15]
+W = data[:,15:18]
+
+# Initial values for MLE
+startVal = np.array([0.5, np.log(0.4), 0.4, 0.8, np.log(1.0), 0.3])
+
+#beta_kappa = .5; sigma_eps = .4; beta_n + pi_ = .4; 
+#gamma1 = .8; sigma_eta = 1; sigma_eps_eta = .3; 
+
+'''Objective function (negative sample log-likelihood function with
+   unconstrained parameters as inputs)
+'''
+
+def objFun(param,Y,Z,Kappa,nn,D,W):
+    
+    '''transfer unconstrained parameters into constrained parameters'''
+    beta_kappa = param[0]
+    sigma_eps = np.exp(param[1])
+    beta_n_pi = param[2]
+    gamma1 = param[3]
+    sigma_eta = np.exp(param[4])
+    sigma_eps_eta = param[5]
+    
+    '''calculate negative sample log-likelihood function'''
+    
+    '''calculate the latent function (decision rule)'''
+    lat = Z*gamma1 - nn*(beta_n_pi) - Kappa*beta_kappa
+ 
+    '''calculate the likelihood if D = 1'''
+    sigma_xi_eta = sigma_eta**2 - sigma_eps_eta
+    sigma_xi = np.sqrt(sigma_eta**2 + sigma_eps**2 - 2*sigma_eps_eta)
+    temp1 = lat + (sigma_xi_eta/(sigma_eta**2))*(W-gamma1*Z)
+    temp2 = np.sqrt(sigma_xi**2 - (sigma_xi_eta**2)/(sigma_eta**2))
+    L1 = (1/sigma_eta)*scipy.stats.norm(0, 1).pdf((W-gamma1*Z)/sigma_eta)*scipy.stats.norm(0,1).cdf(temp1/temp2)
+
+    '''calculate the likelihood if D = 0'''
+    L0 = scipy.stats.norm(0,1).cdf(-lat/sigma_xi)
+
+    '''calculate the negative sample log likelihood'''
+    LL = D*np.log(L1) + (1-D)*np.log(L0)
+    nLL = -sum(LL)
+
+    '''finishing'''
+    return nLL
+    
+''' Maximum Likelihood Estimation
+'''
+sys.stdout = open('Logging.txt', 'a')
+rslt = fmin_bfgs(objFun, startVal, args = (Y,Z,Kappa,nn,D,W))
+sys.stdout = sys.__stdout__
+
+'''Construct dictionary with estimated parameter values
+'''
+mle = {}
+mle['beta_kappa'] = rslt[0]
+mle['sigma_eps'] = np.exp(rslt[1])
+mle['beta_n_pi'] = rslt[2]
+mle['gamma1'] = rslt[3]
+mle['sigma_eta'] = np.exp(rslt[4])
+mle['sigma_esp_eta'] = rslt[5]
+
+with open('MLE_static.json','w') as file_:
+    json.dump(mle,file_)
---
+++ StructuralEstimation/Handout/Solution/Code/static_yike/static_simulation.py
@@ -0,0 +1,67 @@
+''' Simulate the Static Model
+    This project: JG, JEH, YW
+    This code: YW
+    This draft: 20/12/2013
+'''
+
+# Import packages
+import numpy as np
+import os
+
+# Set working directory
+os.chdir('/Users/ywang/Documents/TA/ECON350_Yike/StructuralEstimation/Handout2/Solution/Code/static')
+
+# Set seed
+np.random.seed(0)
+
+# Parameters
+N = 1000; T = 3; beta_kappa = .5; beta_n = .2; sigma_eps = 0.4; pi_ = .2; 
+gamma1 = .8; sigma_eta = 1; sigma_eps_eta = .3; 
+y_lb = 0.0; y_up = 10.0;
+k_lb = 0.0; k_up = 5.0;
+z_lb = 0.0; z_up = 5.0;
+n_lb = 0; n_up = 3
+
+# Simulate observables
+Y = np.random.uniform(low=y_lb,high=y_up,size=(N,T))
+Z = np.zeros((N,T))
+Kappa = np.zeros((N,T))
+nn = np.zeros((N,T))
+Z_temp = np.random.uniform(low=z_lb,high=z_up,size=N)
+Kappa_temp = np.random.uniform(low=k_lb,high=k_up,size=N)
+nn_temp = np.random.random_integers(low=n_lb,high=n_up,size=N)
+for t in range(T):
+    Z[:,t] = Z_temp
+    Kappa[:,t] = Kappa_temp
+    nn[:,t] = nn_temp
+
+# Simulate unobservables
+covs = np.diag([sigma_eta**2,sigma_eps**2])
+covs[0,1] = 0.3
+covs[1,0] = 0.3
+means = [0.0, 0.0]
+unobs = np.random.multivariate_normal(means,covs,(N,T))
+eta = unobs[:,:,0]
+eps = unobs[:,:,1]
+
+'''Simulate individuals' decisions: 
+first calculate U1, U0, and the latent variable function 
+'''
+U1 = Y + gamma1*Z + eta - pi_*nn
+U0 = Y + beta_kappa*Kappa + beta_n*nn + eps
+lat = U1 - U0
+D = lat > 0
+
+'''Simulate individuals' wages:
+note that the wages for women who chose not to work are set to zero
+'''
+W = D*(gamma1*Z + eta)
+
+'''Export sample to *.txt file for further processing. 
+'''
+np.savetxt('data', np.column_stack((Y, Z, Kappa, nn, D, W)), fmt= '%8.3f')
+
+
+
+
+
