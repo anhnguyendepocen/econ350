90d0f9e0a04aad2fb9bc2ce6c13cf46699395737
Jorge <jorge.ggmenendez@gmail.com>
2014-07-06 15:03:05 -0500

    updates

--- StructuralEstimation/Probit/Solution/Code/probit.py
+++ StructuralEstimation/Probit/Solution/Code/probit.py
@@ -1,6 +1,6 @@
 #Solution to Probit exercise
-#This project: JG, JEH, YW
-#This code: JG with edits from JEH
+#This project: JG
+#This code: JG 
 #This draft: 03/01/2014
 
 #Import packages
@@ -12,12 +12,11 @@ np.random.seed(0)
 
 #Exercise 1.3
 
-#=============#
-#Generate data#
-#=============#
+
+#Generate data
 N  = 1000
-X1 = np.random.lognormal(3,1.5,N)
-X2 = np.random.randint(0,2,N)
+X1 = np.random.normal(0,1.5,N)
+X2 = np.random.randint(0,1,N)
 X  = zip(X1,X2)
 E  = np.random.normal(0,1,N)
 
@@ -29,9 +28,8 @@ y_star = Xbeta + E
 #Construct latent and observed decision
 d = y_star >=0
 
-#======================================================================#
-#Define the negative of the likelihood function (the routine minimizes)#
-#======================================================================#
+
+#Define the negative of the likelihood function (the routine minimizes)
 def llk(B):
     XB = np.dot(X,B)
     l_0 = np.log(norm.cdf(-XB))
@@ -43,9 +41,8 @@ def llk(B):
             l_1[i] = np.log(1e-13)
     return -sum(d*l_1 + (1-d)*l_0)
 
-#=========================#
+
 # Solving the minimization#
-#=========================#
 #Initial Condition
 beta0 = np.array([-.3,2.0])
  
